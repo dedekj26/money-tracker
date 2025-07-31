2f59db79811723547a030d0662226a3b6ac16e51
dedekj26 <dedek.julian99@gmail.com>
2025-07-31 14:44:55 +0700

    refactor: comment out unused form elements in edit configuration

--- resources/views/configurations/edit.blade.php
+++ resources/views/configurations/edit.blade.php
@@ -85,7 +85,7 @@
                             </div>
                         </div>
                         
-                        <div class="mb-6">
+                        <!-- <div class="mb-6">
                             <div class="relative flex items-start">
                                 <div class="flex items-center h-5">
                                     <input id="is_active" name="is_active" type="checkbox" value="1" {{ old('is_active', $configuration->is_active) ? 'checked' : '' }}
@@ -96,9 +96,9 @@
                                     <p class="text-gray-500">Periode aktif akan digunakan sebagai filter default untuk laporan dan transaksi.</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         
-                        <div class="bg-yellow-50 p-4 rounded-md mb-6">
+                        <!-- <div class="bg-yellow-50 p-4 rounded-md mb-6">
                             <div class="flex">
                                 <div class="flex-shrink-0">
                                     <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -111,7 +111,7 @@
                                     </p>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         
                         <div class="flex justify-end space-x-3">
                             <a href="{{ route('configurations.index') }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
